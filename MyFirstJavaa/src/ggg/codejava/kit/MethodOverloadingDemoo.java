23248cf9944fa039eadfa2b4ee3bb78a940b5ad7
Gagana M <gaggumohan05@gmail.com>
2026-05-01 12:10:00 +0530

    Methodoverlaoding

---
+++ MyFirstJavaa/src/ggg/codejava/kit/MethodOverloadingDemoo.java
@@ -0,0 +1,12 @@
+package ggg.codejava.kit;
+
+public class MethodOverloadingDemoo {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static String addition(String a, String b) {
+        return a + b;
+    }
+}
---
+++ MyFirstJavaa/src/ggg/codejava/kit/Point.java
@@ -0,0 +1,20 @@
+package ggg.codejava.kit;
+
+public class Point {
+	    int x;
+
+	    // Default constructor
+	    Point() {
+	        x = 0;
+	    }
+
+	    // Parameterized constructor
+	    Point(int x) {
+	        this.x = x;
+	    }
+
+	    public String toString() {
+	        return "Point value: " + x;
+	    }
+	
+}
