0fd9218fb1b87dba5274c54c06f251a9c6dd626f
Gagana M <gaggumohan05@gmail.com>
2026-05-01 12:17:41 +0530

    first commit

--- MyFirstJavaa/src/ggg/codejava/kit/Animal.java
+++ MyFirstJavaa/src/ggg/codejava/kit/Animal.java
@@ -2,9 +2,26 @@ package ggg.codejava.kit;
 
 public abstract class Animal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    abstract void sound();
 
-	}
+    // Normal method
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
 
+class Dog extends Animal {
+
+    // Implement abstract method
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AbstractExample {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.sleep();
+    }
 }
