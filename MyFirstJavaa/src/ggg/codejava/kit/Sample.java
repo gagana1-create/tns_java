7a9265afa44010493f0b459ed47fb581d8cc57cc
Gagana M <gaggumohan05@gmail.com>
2026-05-01 11:48:34 +0530

    sample implements interface

---
+++ MyFirstJavaa/src/ggg/codejava/kit/Sample.java
@@ -0,0 +1,13 @@
+package ggg.codejava.kit;
+
+public class Sample implements InterfaceOne {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void methodOne() {
+        System.out.println("Method One Implementation");
+    }
+}
